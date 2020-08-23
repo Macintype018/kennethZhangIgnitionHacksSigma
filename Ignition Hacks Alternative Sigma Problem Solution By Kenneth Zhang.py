b50d4df06257659a4d9b392d3ab8ae3af8955f85
Macintype018 <50893566+Macintype018@users.noreply.github.com>
2020-08-23 01:20:00 -0400

    Update Ignition Hacks Alternative Sigma Problem Solution By Kenneth Zhang.py

--- Ignition Hacks Alternative Sigma Problem Solution By Kenneth Zhang.py
+++ Ignition Hacks Alternative Sigma Problem Solution By Kenneth Zhang.py
@@ -24,38 +24,39 @@ import re
 # In[4]:
 
 
-data = pd.read_csv('training_data.csv')
-data = data[['Text','Sentiment']]
+trainingData = pd.read_csv('training_data.csv')
+trainingData = trainingData[['Text','Sentiment']]
 
 
 # In[7]:
 
 
-data = data[data.Sentiment != "Neutral"]
-data['Text'] = data['Text'].apply(lambda x: x.lower())
-data['Text'] = data['Text'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
+trainingData = trainingData[trainingData.Sentiment != "Neutral"]
 
-print(data[ data['Sentiment'] == 'Positive'].size)
-print(data[ data['Sentiment'] == 'Negative'].size)
+trainingData['Text'] = trainingData['Text'].apply(lambda x: x.lower())
+trainingData['Text'] = trainingData['Text'].apply((lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
 
-for idx,row in data.iterrows():
+print(trainingData[ trainingData['Sentiment'] == 'Positive'].size)
+print(trainingData[ trainingData['Sentiment'] == 'Negative'].size)
+
+for idx,row in trainingData.iterrows():
     row[0] = row[0].replace('rt',' ')
     
-max_fatures = 2000
-tokenizer = Tokenizer(num_words=max_fatures, split=' ')
-tokenizer.fit_on_texts(data['Text'].values)
-X = tokenizer.texts_to_sequences(data['Text'].values)
+max_features = 2000
+tokenizer = Tokenizer(num_words=max_features, split=' ')
+tokenizer.fit_on_texts(trainingData['Text'].values)
+X = tokenizer.texts_to_sequences(trainingData['Text'].values)
 X = pad_sequences(X)
 
 
 # In[8]:
 
 
-embed_dim = 128
+embdim = 128
 lstm_out = 196
 
 model = Sequential()
-model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+model.add(Embedding(max_features, embdim,input_length = X.shape[1]))
 model.add(SpatialDropout1D(0.4))
 model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(2,activation='softmax'))
@@ -66,7 +67,7 @@ print(model.summary())
 # In[10]:
 
 
-Y = pd.get_dummies(data['Sentiment']).values
+Y = pd.get_dummies(trainingData['Sentiment']).values
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33, random_state = 42)
 print(X_train.shape,Y_train.shape)
 print(X_test.shape,Y_test.shape)
